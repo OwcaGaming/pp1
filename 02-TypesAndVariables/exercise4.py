b87d4b0ce49af6121096120d3fc198bbc2569aaf
Adam Ronkiewicz <ar.ronkiewicz@gmail.com>
2023-10-11 10:43:37 +0200

    my frist python programs from textbook

---
+++ 02-TypesAndVariables/exercise4.py
@@ -0,0 +1,14 @@
+width = 17
+height = 12.0
+
+one = width//2
+print("1. " + str(one) + " " + str(type(one)))
+
+two = width/2.0
+print("2. " + str(two) + " " + str(type(two)))
+
+three = height/3
+print("3. " + str(three) + " " + str(type(three)))
+
+four = (1 + 2 * 5)
+print("4. " + str(four) + " " + str(type(four)))
